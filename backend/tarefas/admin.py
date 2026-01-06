457dc4efad0ef4ea756ae9132c22bd374d0924a4
WellingtonLSoares <wellingtonlima1344@gmail.com>
2026-01-06 05:51:04 -0300

    feat: registra a model Tarefa no django admin

--- backend/tarefas/admin.py
+++ backend/tarefas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tarefa
 
 # Register your models here.
+@admin.register(Tarefa)
+class TarefaAdmin(admin.ModelAdmin):
+  list_display = [field.name for field in Tarefa._meta.fields]
+  list_filter = ('concluida',)
+  search_fields = ('titulo', 'descricao')
