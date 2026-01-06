d5960524bc04668775d87af32bc73ee2d46cc5bb
WellingtonLSoares <wellingtonlima1344@gmail.com>
2026-01-06 06:40:10 -0300

    feat: adiciona mensagens de erro personalizadas para os campos do serializer de tarefas

--- backend/tarefas/serializers.py
+++ backend/tarefas/serializers.py
@@ -5,3 +5,24 @@ class TarefaSerializer(serializers.ModelSerializer):
   class Meta:
     model = Tarefa
     fields = '__all__'
+
+    extra_kwargs = {
+      'titulo': {
+        'error_messages': {
+          'blank': 'O título não pode ficar vazio, por favor preencha.',
+          'required': 'O campo título é obrigatório no JSON.'
+        }
+      },
+      'descricao': {
+        'error_messages': {
+          'blank': 'A descrição não pode ficar vazio, por favor preencha.',
+          'required': 'O campo descrição é obrigatório no JSON.'
+        }
+      },
+      'concluida': {
+        'error_messages': {
+          'invalid': 'Valor inválido! Por favor envie true ou false (booleano).',
+          'null': 'Este campo não pode ser nulo.'
+        }
+      }
+    }
