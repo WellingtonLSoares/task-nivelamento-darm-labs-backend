7ed1863bc25bac48eb4adcff2d2a954cead2d37c
WellingtonLSoares <wellingtonlima1344@gmail.com>
2026-01-06 05:56:56 -0300

    feat: adiciona view para obter todas as tarefas

--- backend/tarefas/views.py
+++ backend/tarefas/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from .models import Tarefa
+from .serializers import TarefaSerializer
+
+import json
 
 # Create your views here.
+@api_view(['GET'])
+def get_all_tarefas(request):
+  tarefas = Tarefa.objects.all()
+  serializer = TarefaSerializer(tarefas, many=True)
+
+  return Response(serializer.data)
+
+
